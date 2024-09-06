d607a593657b3a552acd1467f140d6123f31f8a4
Julian075 <julian.david.7.5@gmail.com>
2024-09-06 00:02:04 -0500

    change dropout in fine tunning

--- main.py
+++ main.py
@@ -125,9 +125,9 @@ if __name__ == "__main__":
                 ruta_features_cis_test = "features/Features_terra/finetuning_features/Features_CATALOG_cis_test_16.pt"
                 ruta_features_trans_test = "features/Features_terra/finetuning_features/Features_CATALOG_trans_test_16.pt"
                 path_text_feat = "features/Features_terra/finetuning_features/Text_features_16.pt"
-
-                model=CATALOG_base_In_domain_terra(weight_Clip=0.6,num_epochs=1000,batch_size=100, num_layers=2,
-                                                dropout=0.27822,hidden_dim=1045,lr=1e-4,t=0.1,momentum=0.8409
+                #dropout=0.27822
+                model=CATALOG_base_In_domain_terra(weight_Clip=0.6,num_epochs=1000,batch_size=100, num_layers=1,
+                                                dropout=0.5,hidden_dim=1045,lr=1e-4,t=0.1,momentum=0.8409
                                                     , patience=20, model=base_fine_tuning, Dataset=TuningDataset,
                                                     Dataloader=dataloader_Tuning, version='fine_tuning',
                                                        ruta_features_train=ruta_features_train,
