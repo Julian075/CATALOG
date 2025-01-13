529cd01cbf35d59c744b5badf1bd765509890aa5
Julian075 <julian.santamaria@udea.edu.com>
2025-01-13 10:57:14 +0100

    LLAMA generation 2 version

--- LLM_generation/LLAMA.py
+++ LLM_generation/LLAMA.py
@@ -2,41 +2,55 @@ from huggingface_hub import login
 import transformers
 import torch
 import os
+import json
+import argparse
 
-# Inicia sesión con tu token de acceso
-token = os.getenv("HF_TOKEN")
-login(token)
-# Configura el modelo y el pipeline
-model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+def generate_species_description(speacies_list):
+    # Inicia sesión con tu token de acceso
+    token = os.getenv("HF_TOKEN")
+    login(token)
+    # Configura el modelo y el pipeline
+    model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto"
-)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model_id,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device_map="auto"
+    )
+    max_length=len(speacies_list)*200
+    # Generación de texto
+    output = pipeline(f"""You are an AI assistant specialized in biology and providing accurate and detailed descriptions of animal species. We
+    are creating detailed and specific prompts to describe various species. The goal is to generate multiple sentences
+    that capture different aspects of each species’ appearance and behavior. Please follow the structure and style shown
+    in the examples below. Each species should have a set of descriptions that highlight key characteristics.
+    Example Structure:
+    Badger:
+    • a badger is a mammal with a stout body and short sturdy legs.
+    • a badger’s fur is coarse and typically grayish-black.
+    • badgers often feature a white stripe running from the nose to
+    the back of the head dividing into two stripes along the sides
+    of the body to the base of the tail.
+    • badgers have broad flat heads with small eyes and ears.
+    • badger noses are elongated and tapered ending in a black
+    muzzle.
+    • badgers possess strong well-developed claws adapted for
+    digging burrows.
+    • overall badgers have a rugged and muscular appearance
+    suited for their burrowing lifestyle.
+    The species are {speacies_list} provide detailed descriptions for each species in the same structure""", max_length=max_length)
+    output_file = "species_descriptions.json"
+    generated_text = output[0]["generated_text"]
+    with open(output_file, "w", encoding="utf-8") as file:
+        json.dump({speacies_list[i]: desc.strip() for i, desc in enumerate(generated_text.split("\n\n"))}, file,
+                  indent=4)
+#speacies_list=['badger', 'bird', 'bobcat', 'car', 'cat', 'coyote', 'deer', 'dog', 'empty', 'fox',  'opossum', 'rabbit', 'raccoon', 'rodent', 'skunk','squirrel']
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract descriptions of animal species.")
+    parser.add_argument("--speacies_list", type=str, nargs='+', required=True, help="Speacies list.")
 
-speacies_list=['badger', 'bird', 'bobcat', 'car', 'cat', 'coyote', 'deer', 'dog', 'empty', 'fox',
-               'opossum', 'rabbit', 'raccoon', 'rodent', 'skunk','squirrel']
-max_length=len(speacies_list)*200
-# Generación de texto
-output = pipeline(f"""You are an AI assistant specialized in biology and providing accurate and detailed descriptions of animal species. We
-are creating detailed and specific prompts to describe various species. The goal is to generate multiple sentences
-that capture different aspects of each species’ appearance and behavior. Please follow the structure and style shown
-in the examples below. Each species should have a set of descriptions that highlight key characteristics.
-Example Structure:
-Badger:
-• a badger is a mammal with a stout body and short sturdy legs.
-• a badger’s fur is coarse and typically grayish-black.
-• badgers often feature a white stripe running from the nose to
-the back of the head dividing into two stripes along the sides
-of the body to the base of the tail.
-• badgers have broad flat heads with small eyes and ears.
-• badger noses are elongated and tapered ending in a black
-muzzle.
-• badgers possess strong well-developed claws adapted for
-digging burrows.
-• overall badgers have a rugged and muscular appearance
-suited for their burrowing lifestyle.
-The species are {speacies_list} give me the descriptions""", max_length=max_length)
-print(output)
+    args = parser.parse_args()
+
+    generate_species_description(args.speacies_list)
+
+
