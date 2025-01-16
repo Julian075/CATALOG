7c04b07bdf3f79a0549ae01e36bc660c64d5d962
Julian075 <julian.santamaria@udea.edu.com>
2025-01-16 11:53:16 +0100

    Ablation tem vs des op3

--- feature_extraction/ablation_category_prompt.py
+++ feature_extraction/ablation_category_prompt.py
@@ -12,13 +12,16 @@ def zeroshot_classifier(classnames, templates1, templates2,omg):
             texts = clip.tokenize(texts).to(device)
             template_embeddings = model_clip.encode_text(texts)  # embed with text encoder
             template_embeddings /= template_embeddings.norm(dim=-1, keepdim=True)
+            template_embeddings = template_embeddings.mean(dim=0)
+            template_embeddings /= template_embeddings.norm(dim=-1, keepdim=True)
 
 
             texts2 = [template for template in templates2[classname]]  # format with class
             texts2 = clip.tokenize(texts2).to(device)
             description_embeddings = model_clip.encode_text(texts2)  # embed with text encoder
             description_embeddings /= description_embeddings.norm(dim=-1, keepdim=True)
-
+            description_embeddings = description_embeddings.mean(dim=0)
+            description_embeddings /= description_embeddings.norm(dim=-1, keepdim=True)
 
             class_embedding = torch.cat ((template_embeddings *(1-omg),description_embeddings * omg), dim = 0)
             class_embedding = class_embedding.mean(dim=0)
@@ -738,10 +741,10 @@ for mode_clip_i in mode_clip:
     model_clip.to(device)
     for omg_i in omg:
         zeroshot_weights = zeroshot_classifier(class_indices_serengeti, camera_trap_templates1, camera_trap_templates_serengeti,omg_i)
-        torch.save(zeroshot_weights,f'features/Features_serengeti/standard_features/Text_{mode_clip_i}_Ab_{omg_i}.pt')
+        torch.save(zeroshot_weights,f'features/Features_serengeti/standard_features/Text_{mode_clip_i}_Ab3_{omg_i}.pt')
 
         zeroshot_weights = zeroshot_classifier(list(class_indices_terra.keys()), camera_trap_templates1, camera_trap_templates_terra,omg_i)
-        torch.save(zeroshot_weights,f'features/Features_terra/standard_features/Text_{mode_clip_i}_Ab_{omg_i}.pt')
+        torch.save(zeroshot_weights,f'features/Features_terra/standard_features/Text_{mode_clip_i}_Ab3_{omg_i}.pt')
 
 
 
