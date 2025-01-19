62508e7120cd2c9bd8111b7d94e24c8dd64e72d3
Julian075 <julian.santamaria@udea.edu.com>
2025-01-19 19:08:19 +0100

    wandb LLMs

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
                     'batch_size': {
                         'distribution': 'int_uniform',
                         'min': 4,
-                        'max': 64
+                        'max': 256
                     },
                     'dropout': {
                         'distribution': 'uniform',
