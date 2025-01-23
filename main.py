cdadb7bac95956f36d796a0b5069b82f2de636af
Julian075 <julian.santamaria@udea.edu.com>
2025-01-23 19:26:31 +0100

    fix the code

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ def mode_model(model,model_params_path,mode):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Program description')
 
+    parser.add_argument('--model_version', type=str, default="Base", help='Model version')
     parser.add_argument('--mode', type=str, default="train", help='define if you want train or test or feature_extraction')
     parser.add_argument('--dataset', type=str, default="serengeti", help='dataset')
-    parser.add_argument('--model_version', type=str, default="Base", help='Model version')
     parser.add_argument('--train_type', type=str, default="In_domain", help='Type of training')
 
 
