a27cb657386c21228884deddc5ba03cdcd369a4c
Julian075 <julian.santamaria@udea.edu.com>
2025-02-17 08:10:29 +0100

    test In Domain

--- train/Fine_tuning/Train_CATALOG_Base_In_domain.py
+++ train/Fine_tuning/Train_CATALOG_Base_In_domain.py
@@ -255,7 +255,7 @@ class CATALOG_base_In_domain:
         dataloader_test = self.dataloader(dataset_D['test'], self.batch_size, self.dataset)
 
 
-        projection_model = self.md.LLaVA_CLIP(hidden_dim=self.hidden_dim, num_layers=self.num_layers, dropout=self.dropout,
+        projection_model = self.md.LLaVA_CLIP_long(hidden_dim=self.hidden_dim, num_layers=self.num_layers, dropout=self.dropout,
                                          device=device)
         projection_model.load_state_dict(torch.load(model_params_path))
         projection_model = projection_model.to(device)
