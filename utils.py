0bdae574bdaae00aa9a9137ae795c250d2e9572d
Julian075 <julian.santamaria@udea.edu.com>
2025-02-24 09:50:54 +0100

    only text

--- utils.py
+++ utils.py
@@ -105,7 +105,7 @@ def feature_extraction_(model_version,dataset,LLM,beta=0.5):
         extract_features(model_version=model_version, dataset=dataset, type_clip='longclip-B', LLM=LLM, only_text=1,beta=beta)
     elif  "BioCLIP" in model_version :
         extract_features(model_version=model_version, dataset=dataset, type_clip='BioCLIP', LLM=LLM, only_text=1,beta=beta)
-    elif not('Long' in model_version):
+    elif not('Long' in model_version) and (model_version != 'Fine_tuning') :
         extract_features(model_version=model_version, dataset=dataset, type_clip='16', LLM=LLM, only_text=1,beta=beta)
     else:
         extract_features(model_version=model_version, dataset=dataset, type_clip='longclip-B', LLM=LLM, only_text=1,beta=beta)
